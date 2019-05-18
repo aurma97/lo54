64e84d6129d24e90d8c031fae353d216ba7fce48
aurma97 <aurelienmarcel77@gmail.com>
2019-05-18 19:38:55 +0200

    First commit

---
+++ src/main/java/com/utbm/fr/lo54/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.utbm.fr.lo54;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
